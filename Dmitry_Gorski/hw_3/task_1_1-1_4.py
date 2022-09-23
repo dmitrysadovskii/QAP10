d933c3234fa25e87b1f2e3ddb54972f95977b025
dhorski <dhorski@gmail.com>
2022-09-23 13:41:22 +0300

    fix flake8 style

--- Dmitry_Gorski/hw_3/task_1_1-1_4.py
+++ Dmitry_Gorski/hw_3/task_1_1-1_4.py
@@ -1,20 +1,14 @@
-""" Заменить символ “#” на символ “/” в строке www.my_site.com#about """
-
-s = 'www.my_site.com#about'
-print(s.replace('#', '/'))
+S = 'www.my_site.com#about'
+print(S.replace('#', '/'))
 
 
-""" Напишите программу, которая добавляет ing к словам """
 def add_ing(s: str) -> str:
-    return ' '.join([ f'{i}ing' for i in s.split(' ')])
+    return ' '.join([f'{i}ing' for i in s.split(' ')])
 
 
-""" В строке Ivanou Ivan поменяйте местами слова Ivan Ivanou """
-
-s = 'Ivanov Ivan'
-print(' '.join(s.split(' ')[::-1]))
+S = 'Ivanov Ivan'
+print(' '.join(S.split(' ')[::-1]))
 
 
-""" Напишите программу которая удаляет пробел в начале, в конце строки """
 def del_space(s: str) -> str:
     return s.strip()
