81b845feed04e0f241c21de83bd9192986ad8a8b
dhorski <dhorski@gmail.com>
2022-09-23 12:27:43 +0300

    some fix in hw_4*

--- Dmitry_Gorski/hw_4/task_with_star.py
+++ Dmitry_Gorski/hw_4/task_with_star.py
@@ -1,13 +1,12 @@
+from collections import Counter as c
+
 """
 Вам передан массив чисел. Известно, что каждое число в этом массиве имеет
 пару, кроме одного: [1, 5, 2, 9, 2, 9, 1] => 5
 Напишите программу, которая будет выводить уникальное число
 """
 
-from collections import Counter
-
-print(''.join([str(k) for k, v in Counter([1, 5, 2, 9, 2, 9, 1]).items() if v == 1]))
-
+print(''.join([str(k) for k, v in c([1, 5, 2, 9, 2, 9, 1]).items() if v == 1]))
 
 """Дан текст, который содержит различные английские буквы и знаки препинания. Вам необходимо найти самую частую букву
 в тексте. Результатом должна быть буква в нижнем регистре. При поиске самой частой буквы, регистр не имеет значения,
@@ -18,7 +17,7 @@ print(''.join([str(k) for k, v in Counter([1, 5, 2, 9, 2, 9, 1]).items() if v ==
 
 sample = 'shdf lsdfJKGTUYKKhFK"LKHaaaaaaksdnbnsafs?>>?<?></)(**^&%^!632328732  sdjfskdRFCNGKJB<M<MN'
 
-d, lst = Counter(list(sample.replace(' ', '').lower())), []
+d, lst = c(list(sample.replace(' ', '').lower())), []
 for k, v in d.items():
     if v == max(d.values()) and k.isalpha():
         lst.append(k)
