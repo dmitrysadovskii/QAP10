07ee0a99bdad5c0afad67d2a6b9c367ab4c37c13
m_mitin <maximus405@gmail.com>
2022-09-24 20:48:24 +0300

    fix task_3_2, format code at PEP8 style

--- Maksim_Mitin/hw_3/task_3_2.py
+++ Maksim_Mitin/hw_3/task_3_2.py
@@ -1,3 +1,5 @@
-word = 'run'
-
-print(word.join(('', 'ing')))
+string = 'run like Forest Gump'
+list_of_words = string.split()
+for i in list_of_words:
+    string_plus_ing = i.join(('', 'ing'))  # можно и сразу на печать выводить просто сделал отдельную переменную
+    print(string_plus_ing)
--- Maksim_Mitin/hw_3/task_3_4.py
+++ Maksim_Mitin/hw_3/task_3_4.py
@@ -1,4 +1,3 @@
 string = '  А я сейчас покажу , откуда готовилось нападение, 4 точки , сейчас покажу, я карту принёс.   '
 string_without_spaces = string.strip()
-
 print(string_without_spaces)
