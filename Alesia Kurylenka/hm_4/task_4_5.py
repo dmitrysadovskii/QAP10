3bb353ebbe916a18ee46638729f01d89c697df6e
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-09-25 20:26:44 +0300

    did task_4_5 and task_4_6 with the generator

--- Alesia Kurylenka/hm_4/task_4_5.py
+++ Alesia Kurylenka/hm_4/task_4_5.py
@@ -1,10 +1,11 @@
 dict_1 = {'a': 1, 'b': 2, 'c': 3}
 dict_2 = {'c': 3, 'd': 4, 'e': 5}
 
-
 dict_1.update(dict_2)
-dict_3 = {}
-for k in dict_1.keys():
-    dict_3[k] = [dict_1.get(k), dict_2.get(k)]
+# dict_3 = {}
+# for k in dict_1.keys():
+#     dict_3[k] = [dict_1.get(k), dict_2.get(k)]
+# print(dict_3)
 
+dict_3 = {k: [dict_1.get(k), dict_2.get(k)] for k in dict_1.keys()}
 print(dict_3)
--- Alesia Kurylenka/hm_4/task_4_6.py
+++ Alesia Kurylenka/hm_4/task_4_6.py
@@ -1,5 +1,8 @@
-a = [1, 5, 2, 9, 2, 9, 1]
+list1 = [1, 5, 2, 9, 2, 9, 1]
 
-for i in a:
-    if a.count(i) == 1:
-        print(i)
+# for i in list1:
+#     if list1.count(i) == 1:
+#         print(i)
+
+list2 = [i for i in list1 if list1.count(i) == 1]
+print(list2)
