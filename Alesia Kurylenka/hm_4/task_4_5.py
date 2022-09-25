1208a6ba36dbd6d2f07fdff5f391617ef9adcfdd
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-09-25 14:37:02 +0300

    added homework4

--- Alesia Kurylenka/hm_4/task_4_5.py
+++ Alesia Kurylenka/hm_4/task_4_5.py
@@ -1,10 +1,10 @@
 dict_1 = {'a': 1, 'b': 2, 'c': 3}
 dict_2 = {'c': 3, 'd': 4, 'e': 5}
 
-dict_1_copy = dict_1.copy()
-dict_1_copy.update(dict_2)
+
+dict_1.update(dict_2)
 dict_3 = {}
-for k in dict_1_copy.keys():
+for k in dict_1.keys():
     dict_3[k] = [dict_1.get(k), dict_2.get(k)]
 
 print(dict_3)
