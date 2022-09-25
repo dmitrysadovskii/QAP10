158e92545acc63ec40020c11373b2e8b4597e6a4
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-09-25 14:13:00 +0300

    added homework4

--- Alesia Kurylenka/hm_4/task_4_5.py
+++ Alesia Kurylenka/hm_4/task_4_5.py
@@ -1,15 +1,10 @@
-dict_1 = { 'a': 1, 'b': 2, 'c': 3}
-dict_2 = { 'c': 3, 'd': 4,'e': 5}
+dict_1 = {'a': 1, 'b': 2, 'c': 3}
+dict_2 = {'c': 3, 'd': 4, 'e': 5}
 
+dict_1_copy = dict_1.copy()
+dict_1_copy.update(dict_2)
+dict_3 = {}
+for k in dict_1_copy.keys():
+    dict_3[k] = [dict_1.get(k), dict_2.get(k)]
 
-def mergeDictionary(dict_1, dict_2):
-   dict_3 = {**dict_1, **dict_2}
-   for key, value in dict_3.items():
-       if key in dict_1 and key in dict_2:
-               dict_3[key] = [value , dict_1[key]]
-       else:
-           dict_3[key] = [value, None]
-   return dict_3
-
-dict_3 = mergeDictionary(dict_1, dict_2)
 print(dict_3)
--- Alesia Kurylenka/hm_4/task_4_7.py
+++ Alesia Kurylenka/hm_4/task_4_7.py
@@ -4,5 +4,5 @@ a1 = (''.join(filter(str.isalpha, a)))
 a1_low = a1.lower()
 a1_low_sorted = sorted(a1_low)
 
-a2 = max(a1_low_sorted, key = a1_low_sorted.count)
+a2 = max(a1_low_sorted, key=a1_low_sorted.count)
 print(a2)
