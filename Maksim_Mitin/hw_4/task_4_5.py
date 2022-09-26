8ba09e69711a5aeafbb36fb41e4c2dea0f6303fe
m_mitin <maximus405@gmail.com>
2022-09-27 00:51:19 +0300

    format to pep8

--- Maksim_Mitin/hw_4/task_4_5.py
+++ Maksim_Mitin/hw_4/task_4_5.py
@@ -1,18 +1,16 @@
-a = {'a': 1, 'b': 2, 'c': 3}
-b = {'c': 3, 'd': 4, 'e': 5}
+a = {"a": 1, "b": 2, "c": 3}
+b = {"c": 3, "d": 4, "e": 5}
 ab = a | b
 
 for k, v in b.items():
     if a.get(k):
         ab[k] = [a[k], v]
-        # b[k] = [b[k], v]
     else:
         ab[k] = [None, v]
 
 for k, v in a.items():
     if b.get(k):
         ab[k] = [b[k], v]
-        # b[k] = [b[k], v]
     else:
         ab[k] = [v, None]
 
