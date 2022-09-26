712932675536f78de1213eb3ba401b754fb568c5
Elena Rybina <elenarybinastudio@gmail.com>
2022-09-25 17:50:54 -0700

    add hm3

---
+++ Elena Rybina/hm_3/task_3_2.py
@@ -0,0 +1,7 @@
+end = 'ing'
+var = 'work' + end
+var2 = 'cry' + end
+var3 = 'dream' + end
+print(var, var2, var3)
+
+
---
+++ Elena Rybina/hm_3/task_3_4.py
@@ -0,0 +1,7 @@
+str1 = ' Holly Molly Guacamole '
+str2 = len(str1)
+print(str2)
+# считаем сколько символов
+
+res_str = str1[1:22]
+print(res_str)
