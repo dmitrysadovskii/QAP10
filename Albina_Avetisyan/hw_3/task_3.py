040c4bedbf72dcda4ee2d911cf4eaf9a87cbe8f7
Albina Avetisyan <Albina.Avetisyan.workmail@gmail.com>
2022-09-29 00:52:05 +0300

    add homework 3

---
+++ Albina_Avetisyan/hw_3/task_3.py
@@ -0,0 +1,19 @@
+print('zadanie 1 =)')
+expression = 'www.my_site.com#about'
+print(expression.replace('#', '/'))
+
+print('zadanie 2 =)')
+expression = 'There is a great meme that sums it up'
+print(expression.replace(' ', 'ing ') + "ing")
+
+print('zadanie 3 =)')
+expression = "Ivanou Ivan"
+r = expression.split(' ')
+r.reverse()
+print(' '.join(r))
+
+print('zadanie 4 =)')
+expression = ' ' + expression + '  '
+print(expression.strip())
+
+
