55c663808fd14e85eb3a6c04eac7c73083d6c8b5
Darya Huzar <darya.huzar@gmail.com>
2022-10-01 12:45:49 +0300

    corrected task_3_2

--- Darya Huzar/hw_3/task_3_2.py
+++ Darya Huzar/hw_3/task_3_2.py
@@ -1,4 +1,5 @@
-# enter any word in the console
-S = input()
-S_new = S + 'ing'
-print(S_new)
+# enter a string in the console
+old_str = input()
+new_str = ' '.join(f'{word}ing' for word in old_str.split())
+print(new_str)
+
