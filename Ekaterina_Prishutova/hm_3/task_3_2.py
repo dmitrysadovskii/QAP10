24c20a3e64154375abcf82b1a003f4a36bf98f08
Ekaterina Prishutova <prishutova.ea@mail.ru>
2022-10-01 13:13:52 +0300

    hm_3 task_3_2

--- Ekaterina_Prishutova/hm_3/task_3_2.py
+++ Ekaterina_Prishutova/hm_3/task_3_2.py
@@ -1,3 +1,5 @@
-word = "work"
-word_mod = word + "ing"
-print(word_mod)
+words = " ololo nanan b  jojojo "
+words_mod = words.strip().split(" ")
+words_mod_2 = "ing ".join(words_mod) + 'ing'
+print(words_mod)
+print(words_mod_2)
