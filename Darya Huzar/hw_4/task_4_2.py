771978914f944f8b170c2ec2f984efed78624fef
Darya Huzar <darya.huzar@gmail.com>
2022-10-02 22:30:00 +0300

    homework 4

---
+++ Darya Huzar/hw_4/task_4_2.py
@@ -0,0 +1,4 @@
+name = ['Ivan', 'Ivanou']
+city  = 'Minsk'
+country = 'Belarus'
+print(f'Привет, {name[0]} {name[1]}! Добро пожаловать в {city}, {country}.')
---
+++ Darya Huzar/hw_4/task_4_6.py
@@ -0,0 +1,6 @@
+nums = [1, 5, 2, 9, 2, 9, 1]
+unique = []
+for i in nums:
+     if nums.count(i) == 1:
+           unique.append(i)
+print(*unique)
