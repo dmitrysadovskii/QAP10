beae7fc99cf21d8f5b16249fe91cba3a8703423c
Darya Huzar <darya.huzar@gmail.com>
2022-10-02 22:36:10 +0300

    corrected mistakes

--- Darya Huzar/hw_4/task_4_2.py
+++ Darya Huzar/hw_4/task_4_2.py
@@ -1,4 +1,4 @@
 name = ['Ivan', 'Ivanou']
-city  = 'Minsk'
+city = 'Minsk'
 country = 'Belarus'
 print(f'Привет, {name[0]} {name[1]}! Добро пожаловать в {city}, {country}.')
--- Darya Huzar/hw_4/task_4_5.py
+++ Darya Huzar/hw_4/task_4_5.py
@@ -1,6 +1,6 @@
 a = { 'a': 1, 'b': 2, 'c': 3}
 b = { 'c': 3, 'd': 4, 'e': 5}
-ab = {}
+ab = { }
 for k, v in {**a, **b}.items():
     if k in a and k in b:
         ab.update({k: [a[k], b[k]]})
