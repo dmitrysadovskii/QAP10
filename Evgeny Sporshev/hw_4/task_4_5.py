dc112d28e07de03be242409f2f3dd3f78ebc8c6f
Evgeny Sporshev <verygnawtyatwork@yandex.ru>
2022-10-02 03:40:40 +0300

    adding get() function to task_4_5

--- Evgeny Sporshev/hw_4/task_4_5.py
+++ Evgeny Sporshev/hw_4/task_4_5.py
@@ -4,11 +4,6 @@ ab = {}
 # ab = {'a': [1, None], 'b': [2, None], 'c': [3, 3], 'd': [None, 4], 'e': [None, 5]}
 
 for k, v in {**a, **b}.items():
-    if k in a and k in b:
-        ab.update({k: [a[k], b[k]]})
-    elif k in a and k not in b:
-        ab.update({k: [v, None]})
-    else:
-        ab.update({k: [None, v]})
+    ab.update({k: [a.get(k), b.get(k)]})
 
 print(ab)
