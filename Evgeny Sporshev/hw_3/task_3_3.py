852b021132a8b0e7e905b06a70e51a956d45006f
verygnawtyatwork <69771592+verygnawtyatwork@users.noreply.github.com>
2022-10-03 19:11:47 +0300

    Delete task_3_3.py
    Delete useless files.

--- Evgeny Sporshev/hw_3/task_3_3.py
+++
@@ -1,6 +0,0 @@
-test_string = 'Ivan Ivanou'
-test_string = test_string.split()
-test_string.reverse()
-# А вот тут хочется задать вопрос: почему не работает, если написать test_string = test_string.reverse()?
-# Хотя со split() присвоение работает
-print(test_string)
