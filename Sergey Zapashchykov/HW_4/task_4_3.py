c3f2b4d37711ab556734110c11c4b4beaaab4d9f
Sergey Zapashchykov <sergey.zapashchykov@gmail.com>
2022-10-04 00:58:21 +0300

    Sergey_Zapashchykov: Homework4

---
+++ Sergey Zapashchykov/HW_4/task_4_3.py
@@ -0,0 +1,3 @@
+list1 = ["I", "love", "arrays", "they", "are", "my", "favorite"]
+text1 = ' '.join(list1)
+print(text1)
---
+++ Sergey Zapashchykov/HW_4/task_4_7.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+text = 'a' * 9000 + 'b' * 9000 + ',' * 500 + '   ' * 20 + '1'* 20 + 'n' * 9000
+text_to_list = list()
+
+for i in text.lower():
+    if i.isalpha():
+        text_to_list.append(i)
+
+c = Counter(text_to_list)
+print(c)
+v = c.most_common(1)
+print(v)
+w = v[0][0]
+print(f'Самая частая буква: {w}')
