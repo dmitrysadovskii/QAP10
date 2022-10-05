ae336a3f9e193835ff33400240bb901aa8656a3e
Raikhelson <valerie.test.dan@gmail.com>
2022-10-05 16:03:29 +0300

    changes in task_4_5 and task_4_6

--- Valerie_Raikhelson/hm_4/task_4_5.py
+++ Valerie_Raikhelson/hm_4/task_4_5.py
@@ -1,19 +1,13 @@
 a = {'a': 1, 'b': 2, 'c': 3}
 b = {'c': 3, 'd': 4, 'e': 5}
+c = a | b
 
+print(c.keys())
 for k, v in a.items():
     for kk, vv in b.items():
-        if k == kk:
-            print(k)
-            a[k] = [a[k], b[kk]]
-            b[kk] = a[k]
-        if k != kk:
-            a[k] = [a[k], 'none']
-        break
+        c[k] = [a.get(k), b.get(k)]
 
 for k, v in b.items():
-    if k != 'c':
-        print(k, v)
-        b[k] = ['none', b[k]]
-
-print(a | b)
+    for kk, vv in a.items():
+        c[k] = [a.get(k), b.get(k)]
+print(c)
--- Valerie_Raikhelson/hm_4/task_4_6.py
+++ Valerie_Raikhelson/hm_4/task_4_6.py
@@ -1,8 +1,10 @@
 from collections import Counter
 
-numbers = [1, 5, 2, 9, 2, 9, 1]
+numbers = [1, 5, 5, 2, 9, 2, 9, 1]
 el_count = Counter(numbers)
 
+print(el_count)
 for num, count in el_count.items():
     if count == min(el_count.values()):
-        print(num)
+        if count == 1:
+            print(num)
