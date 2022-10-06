405fd7e1edab378df9a392c5fb4388f8187dcefb
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-10-06 15:43:51 +0300

    fixed tast_5_2 with BuzzFuzz, fixed task_5_3 with additional checks

--- Alesia Kurylenka/hw_5/task_5_3.py
+++ Alesia Kurylenka/hw_5/task_5_3.py
@@ -1,11 +1,17 @@
 import random
 
 # 1 case: game with person
-number1 = input("Please enter a 4-digit number with non-repetitive digits: ")
+number1 = "1234"
 while True:
-    number2 = input("Please try to guess the secret number, enter a 4-digit number with non-repetitive digits: ")
     bulls = 0
     cows = 0
+    number2 = input("Please try to guess the secret number, enter a 4-digit number with non-repetitive digits: ")
+    if len(number2) != 4:
+        print("enter a 4-digit number")
+        continue
+    elif len(set(number2)) != 4:
+        print("only non-repetitive digits!")
+        continue
     for i in range(4):
         if number1[i] == number2[i]:
             bulls += 1
@@ -16,14 +22,20 @@ while True:
         print("you won the game!")
         break
 
-# 2 case: game with computer
 
+# 2 case: game with computer
 number_rand = random.sample("1234567890", 4)
 number_rand_int = "".join(number_rand)
 while True:
     number = input("Please try to guess the secret number, enter a 4-digit number with non-repetitive digits: ")
     bulls = 0
     cows = 0
+    if len(number) != 4:
+        print("enter a 4-digit number")
+        continue
+    elif len(set(number)) != 4:
+        print("only non-repetitive digits!")
+        continue
     for i in range(4):
         if number[i] == number_rand_int[i]:
             bulls += 1
