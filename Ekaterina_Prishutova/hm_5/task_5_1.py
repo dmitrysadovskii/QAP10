2ec620a730d9153d8fe4cfd944f12a85341ea54c
Ekaterina Prishutova <prishutova.ea@mail.ru>
2022-10-06 13:57:08 +0300

    homework 5: fix task_5_1

--- Ekaterina_Prishutova/hm_5/task_5_1.py
+++ Ekaterina_Prishutova/hm_5/task_5_1.py
@@ -1,25 +1,26 @@
 import langid
 
 str_names = input("Введите имена в формате: Анна, Евгений, Федор, Глафира \n").split(", ")
+
 if len(str_names) == 0:
     print("no one likes this")
-elif len(str_names) == 1:
-    if langid.classify(str_names[0])[0] == "en":
+elif langid.classify(str_names[0])[0] == "en":
+    if len(str_names) == 1:
         print(f"{str_names[0]} likes this")
-    elif langid.classify(str_names[0])[0] == "ru":
-        print(f"{str_names[0]} оценил(а) запись")
-elif len(str_names) == 2:
-    if langid.classify(str_names[0])[0] == "en":
+    elif len(str_names) == 2:
         print(f"{str_names[0]} and {str_names[1]} likes this")
-    elif langid.classify(str_names[0])[0] == "ru":
-        print(f"{str_names[0]} и {str_names[1]} оценили запись")
-elif len(str_names) == 3:
-    if langid.classify(str_names[0])[0] == "en":
+    elif len(str_names) == 3:
         print(f"{str_names[0]}, {str_names[1]} and {str_names[2]} likes this")
-    elif langid.classify(str_names[0])[0] == "ru":
+    else:
+        print(f"{str_names[0]}, {str_names[1]} and {len(str_names) - 2} others like this")
+elif langid.classify(str_names[0])[0] == "ru":
+    if len(str_names) == 1:
+        print(f"{str_names[0]} оценил(а) запись")
+    elif len(str_names) == 2:
+        print(f"{str_names[0]} и {str_names[1]} оценили запись")
+    elif len(str_names) == 3:
         print(f"{str_names[0]}, {str_names[1]} и {str_names[2]} оценили запись")
-else:
-    if langid.classify(str_names[0])[0] == "en":
-        print(f"{str_names[0]}, {str_names[1]} and {len(str_names)-2} others like this")
-    elif langid.classify(str_names[0])[0] == "ru":
+    else:
         print(f"{str_names[0]}, {str_names[1]} и еще {len(str_names)-2} оценили запись")
+else:
+    print("Язык не определен")
