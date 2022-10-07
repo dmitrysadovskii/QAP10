18b49d48f076065f358916ef7fbba9ab04fcff0c
Evgeny Sporshev <verygnawtyatwork@yandex.ru>
2022-10-07 18:23:41 +0300

    make improvements to task_5_1 and task_5_3

--- Evgeny Sporshev/hw_5/task_5_1.py
+++ Evgeny Sporshev/hw_5/task_5_1.py
@@ -1,25 +1,22 @@
-likes_list = ["Ann", "Alex", "Mark", "Max"]
-russian_letters = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л',
-                   'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю']
-if likes_list == []:
-    print('no one likes this')
-elif len(likes_list) == 1:
-    if likes_list[0][0].lower() in russian_letters:
-        print(likes_list[0], 'любит это')
-    else:
+likes_list = ['Ann', 'Bann', 'Cann', 'Dann']
+
+if len(likes_list) == 0:
+    print('No one likes this')
+elif len(likes_list[0]) == len(likes_list[0].encode()):
+    if len(likes_list) == 1:
         print(likes_list[0], 'likes this')
-elif len(likes_list) == 2:
-    if likes_list[0][0].lower() in russian_letters:
-        print(f'{likes_list[0]} и {likes_list[1]} любят это')
-    else:
+    elif len(likes_list) == 2:
         print(f'{likes_list[0]} and {likes_list[1]} like this')
-elif len(likes_list) == 3:
-    if likes_list[0][0].lower() in russian_letters:
-        print(f'{likes_list[0]}, {likes_list[1]} и {likes_list[2]} любят это')
-    else:
+    elif len(likes_list) == 3:
         print(f'{likes_list[0]}, {likes_list[1]} and {likes_list[2]} like this')
-else:
-    if likes_list[0][0].lower() in russian_letters:
-        print(f'{likes_list[0]}, {likes_list[1]} и {len(likes_list) - 2} других любят это')
     else:
         print(f'{likes_list[0]}, {likes_list[1]} and {len(likes_list) - 2} others like this')
+else:
+    if len(likes_list) == 1:
+        print(likes_list[0], 'любит это')
+    elif len(likes_list) == 2:
+        print(f'{likes_list[0]} и {likes_list[1]} любят это')
+    elif len(likes_list) == 3:
+        print(f'{likes_list[0]}, {likes_list[1]} и {likes_list[2]} любят это')
+    else:
+        print(f'{likes_list[0]}, {likes_list[1]} и {len(likes_list) - 2} других любят это')
--- Evgeny Sporshev/hw_5/task_5_3.py
+++ Evgeny Sporshev/hw_5/task_5_3.py
@@ -1,8 +1,34 @@
-secret_number = input('Игрок 1, загадай четырехзначное число число: ')
+def len_checker(a):
+    if len(str(a)) != 4:
+        return False
+    else:
+        return True
+
+
+def repeat_checker(c):
+    c = str(c)
+    i = 0
+    while i < len(c):
+        if c.count(c[i]) > 1:
+            return False
+        else:
+            i += 1
 
 
 while True:
-    suggested_number = input('Игрок 2, угадай четырехзначное число число: ')
+    secret_number = input('Игрок 1, загадай четырехзначное число число: ')
+    if len_checker(secret_number) is True and repeat_checker(secret_number) is None:
+        break
+    else:
+        print('Ошибка, должно быть четырехзначное число с неповторяющимися числами')
+
+while True:
+    while True:
+        suggested_number = input('Игрок 2, угадай четырехзначное число число: ')
+        if len_checker(suggested_number) is True and repeat_checker(suggested_number) is None:
+            break
+        else:
+            print('Ошибка, должно быть четырехзначное число с неповторяющимися числами')
     cow_counter = 0
     bull_counter = 0
     if suggested_number == secret_number:
