ac6a17423420bf15cb7748643696bb6315058f6c
Anton Lepesiy <anton.lepesiy@gmail.com>
2022-10-08 20:06:23 +0200

    flake8 errors in task_4_2, task_4_4 corrected

--- Anton Lepesiy/hw_4/task_4_2.py
+++ Anton Lepesiy/hw_4/task_4_2.py
@@ -6,7 +6,5 @@ country = 'Belsrus'
 a = ' '.join(name)  # преобразование списка в строку
 
 print('Привет,' + a + '! ' + 'Добро пожаловать в ' + city + ' ' + country)
-
-    # metod 2 :
+# metod 2:
 print('Привет, {name}! Добро пожаловать в {city} {country}'.format(name=a, city="Minsk", country='Belsrus'))
-
