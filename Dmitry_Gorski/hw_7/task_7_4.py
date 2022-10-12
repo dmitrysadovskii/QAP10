1e0d0b36afaaa5aafd69285f6a8e81763b71cb60
dhorski <dhorski@gmail.com>
2022-10-12 16:53:31 +0300

    fix after review

--- Dmitry_Gorski/hw_7/task_7_4.py
+++ Dmitry_Gorski/hw_7/task_7_4.py
@@ -1,7 +1,4 @@
-def cesar_coding(in_str: str, bias: int, lang='eng') -> str:
-    assert lang in ('eng', 'rus'), 'You can use only ENG|RUS language'
-
-    out_string = str()
+def init_constant(lang: str) -> dict:
     if lang == 'eng':
         upper_letter, lower_letter = 'A', 'a'
         amount_letter = 26
@@ -9,11 +6,23 @@ def cesar_coding(in_str: str, bias: int, lang='eng') -> str:
         upper_letter, lower_letter = 'А', 'а'
         amount_letter = 32
 
+    return {'l': lang,
+            'ul': upper_letter,
+            'll': lower_letter,
+            'al': amount_letter}
+
+
+def cesar_coding(in_str: str, bias: int, lang: str) -> str:
+    assert lang in ('eng', 'rus'), 'You can use only ENG|RUS language'
+
+    init = init_constant(lang)
+
+    out_string = str()
     for i in in_str:
         if i.isupper():
-            out_string += chr((ord(i) - ord(upper_letter) + bias) % amount_letter + ord(upper_letter))
+            out_string += chr((ord(i) - ord(init.get('ul')) + bias) % init.get('al') + ord(init.get('ul')))
         elif i.islower():
-            out_string += chr((ord(i) - ord(lower_letter) + bias) % amount_letter + ord(lower_letter))
+            out_string += chr((ord(i) - ord(init.get('ll')) + bias) % init.get('al') + ord(init.get('ll')))
         elif i.isdigit():
             out_string += str((int(i) + bias) % 10)
         else:
@@ -28,19 +37,14 @@ print(cesar_coding('Привет Мир!', 4, 'rus'))
 def cesar_decoding(in_str: str, bias: int, lang='eng') -> str:
     assert lang in ('eng', 'rus'), 'You can use only ENG|RUS language'
 
-    out_string = str()
-    if lang == 'eng':
-        upper_letter, lower_letter = 'A', 'a'
-        amount_letter = 26
-    else:
-        upper_letter, lower_letter = 'А', 'а'
-        amount_letter = 32
+    init = init_constant(lang)
 
+    out_string = str()
     for i in in_str:
         if i.isupper():
-            out_string += chr((ord(i) - ord(upper_letter) - bias) % amount_letter + ord(upper_letter))
+            out_string += chr((ord(i) - ord(init.get('ul')) - bias) % init.get('al') + ord(init.get('ul')))
         elif i.islower():
-            out_string += chr((ord(i) - ord(lower_letter) - bias) % amount_letter + ord(lower_letter))
+            out_string += chr((ord(i) - ord(init.get('ll')) + bias) % init.get('al') + ord(init.get('ll')))
         elif i.isdigit():
             out_string += str((int(i) - bias) % 10)
         else:
@@ -56,20 +60,24 @@ print(cesar_decoding('Уфмжйц Рмф!', 4, 'rus'))
 
 class CesarCodeDecode:
 
-    def __init__(self, str_in, bias, lang):
+    def __init__(self, str_in, bias):
         self.str_in = str_in
         self.bias = bias
-        self.lang = lang
 
-    def coding(self):
-        assert self.lang in ('eng', 'rus'), 'You can use only ENG|RUS language'
-
-        if self.lang == 'eng':
+    @staticmethod
+    def install_vars(lang) -> tuple:
+        if lang == 'eng':
             upper, lower = 'A', 'a'
             amount = 26
         else:
             upper, lower = 'А', 'а'
             amount = 32
+        return upper, lower, amount
+
+    def coding(self, lang):
+        assert lang in ('eng', 'rus'), 'You can use only ENG|RUS language'
+
+        upper, lower, amount = self.install_vars(lang)
 
         out_string = str()
         for i in self.str_in:
@@ -83,13 +91,13 @@ class CesarCodeDecode:
                 out_string += i
         return out_string
 
-    def decoding(self):
+    def decoding(self, lang):
         self.bias = - self.bias
-        out_string = self.coding()
+        out_string = self.coding(lang)
         return out_string
 
 
-print(CesarCodeDecode('Hello World!', 4, 'eng').coding())
-print(CesarCodeDecode('Привет Мир!', 4, 'rus').coding())
-print(CesarCodeDecode('Lipps Asvph!', 4, 'eng').decoding())
-print(CesarCodeDecode('Уфмжйц Рмф!', 4, 'rus').decoding())
+print(CesarCodeDecode('Hello World!', 4).coding('eng'))
+print(CesarCodeDecode('Привет Мир!', 4).coding('rus'))
+print(CesarCodeDecode('Lipps Asvph!', 4).decoding('eng'))
+print(CesarCodeDecode('Уфмжйц Рмф!', 4).decoding('rus'))
