ea576a4d2260b66c766ee0d6d75396f4c0a07b91
Raikhelson <114160353+Raikhelson@users.noreply.github.com>
2022-10-14 23:29:30 +0300

    Delete Valerie_Raikhelson/hm_7 directory

--- Valerie_Raikhelson/hm_7/task_7_3.py
+++
@@ -1,4 +0,0 @@
-sentence = " thequick brown fox jumps over the lazy dog"
-list_of_word_lengths = []
-[list_of_word_lengths.append(len(word)) for word in sentence.replace("the", "").split(" ") if len(word) > 0]
-print(list_of_word_lengths)
