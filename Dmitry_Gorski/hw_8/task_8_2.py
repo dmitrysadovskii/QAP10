c14a48f48f9908d3b139786d4ecbc1db3c68ecf8
dhorski <dhorski@gmail.com>
2022-10-14 09:05:55 +0300

    flake8 fix

--- Dmitry_Gorski/hw_8/task_8_2.py
+++ Dmitry_Gorski/hw_8/task_8_2.py
@@ -6,7 +6,7 @@ number_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', '
 def check_args(*args):
     assert (isinstance(arg, (int, float)) for arg in args), 'All args must be int or float'
     assert (0 <= arg < 20 for arg in args), 'All args must be in range [0; 20)'
-    assert len(args) <= 100, f'The total number of args must not gt 100'
+    assert len(args) <= 100, 'The total number of args must not gt 100'
 
 
 def lexicographic_sort(*args):
