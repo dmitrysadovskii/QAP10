5e85728aeb1301ee4bf9822cfcb549595bb88a16
dhorski <dhorski@gmail.com>
2022-10-14 10:53:56 +0300

    add functools wraps

--- Dmitry_Gorski/hw_8/task_8_1.py
+++ Dmitry_Gorski/hw_8/task_8_1.py
@@ -1,15 +1,26 @@
+from functools import wraps
+
+
 def typed(type_in):
     assert type_in in ('str', 'int', 'float'), 'Only (int, str, float) type you can use'
 
     def decorator(func):
+        @wraps(func)
         def wrapper(*args):
             return func(*list(map(getattr(__builtins__, type_in), args)))
+
         return wrapper
+
     return decorator
 
 
 @typed(type_in='float')
 def test_function(*args):
+    """
+    func for addition input data
+    :param args:  int, float, str
+    :return: int, float, str
+    """
     try:
         return sum([*args])
     except TypeError:
