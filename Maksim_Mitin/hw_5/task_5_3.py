1941e74193d544205c697cf7e360f705b41ff1e2
m_mitin <maximus405@gmail.com>
2022-10-15 20:37:29 +0300

    add hm5

--- Maksim_Mitin/hw_5/task_5_3.py
+++ Maksim_Mitin/hw_5/task_5_3.py
@@ -1,11 +1,25 @@
-import random
-# делал через генерацию рандомных чисел, по идее можно через инпут запрашивать число и его аргументом в функцию
-# (но там надо проверки делать на кратность4 и без симвлов и букв,я подумал надо сделать просто)
+number = input('Введите 4 значное число без повторных цифр - ')
+
+if len(number) != 4:
+    print("введено неправлиьное число, введено больше или меньше 4 чисел")
+    number = input('Введите 4 значное число без повторных цифр - ')
+
+list_check = [int(n) for n in number]
+uniquelist = []
+
+for item in list_check:
+    itemExist = False
+    for x in uniquelist:
+        if x == item:
+            itemExist = True
+            print("введено неправлиьное число, числа повторяются")
+            number = input('Введите 4 значное число без повторных цифр - ')
+    if not itemExist:
+        uniquelist.append(item)
 
 
-def cows_and_bulls():
-    listnumbers = [random.randint(0, 9) for n in range(4)]
-
+def cows_and_bulls(some_numbers):
+    listnumbers = [int(n) for n in some_numbers]
     while True:
         print(f"Загадано число {str(listnumbers)}")
         guess = [int(i) for i in str(input())]
@@ -17,12 +31,15 @@ def cows_and_bulls():
             cow = 0
             bull = 0
 
-            for x in range(0, 3):
+            for x in range(len(listnumbers)):
                 if guess[x] == listnumbers[x]:
-                    cow += 1
-                elif guess[x] in listnumbers:
                     bull += 1
+                elif guess[x] in listnumbers:
+                    cow += 1
+                else:
+                    continue
+
         print(f"Коров = {str(cow)}. Быков = {str(bull)}")
 
 
-cows_and_bulls()
+cows_and_bulls(number)
