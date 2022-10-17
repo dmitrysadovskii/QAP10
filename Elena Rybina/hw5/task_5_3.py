b1e7f6f4d4e402895aa101776bdd8b90f7e0d282
Elena Rybina <elenarybinastudio@gmail.com>
2022-10-16 18:21:31 -0700

    add hw5

---
+++ Elena Rybina/hw5/task_5_3.py
@@ -0,0 +1,33 @@
+import random
+
+secret_number = str(random.randint(1000, 9999))
+print("Guess the number. It contains 4 digits.")
+
+
+while True:
+    cows=0
+    bulls=0
+
+    player_guess = input("Enter your guess: ")
+
+
+    if len(player_guess)!=4:
+        print('Incorrect input!')
+        break
+
+    else:
+
+        if player_guess[0] == secret_number[0]:
+            bulls += 1
+        if player_guess[1] == secret_number[1]:
+            bulls += 1
+        if player_guess[0] == secret_number[1]:
+            cows += 1
+        if player_guess[1] == secret_number[0]:
+            cows += 1
+
+        print("Bulls: ", bulls)
+        print("Cows: ", cows)
+
+        if bulls==4:
+            print('Yay, You Won!')
