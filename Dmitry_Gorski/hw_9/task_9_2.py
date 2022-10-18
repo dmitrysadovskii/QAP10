2eacb577e2eb00cbdbaac905b2411877e4216eb9
dhorski <dhorski@gmail.com>
2022-10-18 15:24:16 +0300

    add profit in answer

--- Dmitry_Gorski/hw_9/task_9_2.py
+++ Dmitry_Gorski/hw_9/task_9_2.py
@@ -11,11 +11,12 @@ class Bank:
 
     @staticmethod
     def deposit_info():
+        start_invest_summa = invest.summa
         for i in range(1, invest.period * 12 + 1):
             invest.summa = invest.summa + (invest.summa * Investment.INVESTMENT_PERCENT / 1200)
-        return invest.summa
+        return invest.summa - start_invest_summa
 
 
-invest = Investment(10000, 18)
+invest = Investment(10000, 1)
 wallet = Bank
-print(f'Total money: {wallet.deposit_info()}')
+print(f'Total profit: {wallet.deposit_info()}')
