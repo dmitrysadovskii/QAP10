3c209b64568ce7bc483361d0b3906727874d53d9
Anton Lepesiy <anton.lepesiy@gmail.com>
2022-10-20 10:44:56 +0200

    удалены лишние комменты, исправлен вывод результата для 1 имени

--- Anton Lepesiy/hw_5/task_5_1.py
+++ Anton Lepesiy/hw_5/task_5_1.py
@@ -1,18 +1,19 @@
 likes = ("Ann", "Alex", "Mark", "Max")
-# likes = "Ann"
-# likes = ("Ann", "Alex")
-# likes = ("Ann", "Alex", "Mark")
+
 a = len(likes)
 
-print('Колличество лайков: ', a)
 
 if likes == 0:
     print("no one likes this.")
 elif type(likes) == str:
+    print('Колличество лайков: 1')
     print(f" {likes} like this.")
 elif a == 2:
+    print('Колличество лайков: ', a)
     print(" and ".join(likes), "like this.")
 elif a == 3:
+    print('Колличество лайков: ', a)
     print(likes[0], ',', likes[1], "and", likes[2], 'like this.')
 elif a >= 4:
+    print('Колличество лайков: ', a)
     print(likes[0], ',', likes[1], 'and 2 others like this.')
