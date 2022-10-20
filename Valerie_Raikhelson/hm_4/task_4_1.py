059216f6a1df70ac284d1d1c77695674a3e52561
Raikhelson <114160353+Raikhelson@users.noreply.github.com>
2022-10-20 04:55:48 +0300

    Delete Valerie_Raikhelson/hm_4 directory

--- Valerie_Raikhelson/hm_4/task_4_1.py
+++
@@ -1,4 +0,0 @@
-str_1 = 'Robin Singh'
-str_2 = 'I love arrays they are my favorite'
-print(str_1.split(' '))
-print(str_2.split(' '))
--- Valerie_Raikhelson/hm_4/task_4_2.py
+++
@@ -1,5 +0,0 @@
-name = ['Ivan', 'Ivanou']
-city = 'Minsk'
-country = 'Belarus'
-output = 'Привет, ' + name[0] + ' ' + name[1] + '! Добро пожаловать в ' + city + ' ' + country
-print(output)
