d04bce98f9fc34d2d6a47413719488cf12584e8e
Anton Lepesiy <anton.lepesiy@gmail.com>
2022-10-26 21:15:18 +0200

    home work 8 added

---
+++ Anton Lepesiy/hw_8/task_8_1.py
@@ -0,0 +1,38 @@
+# Напишите декоратор, который проверял бы тип параметров функции, конвертировал их если надо и складывал:
+
+
+def typed_func(type):
+    def wrapper(function):
+        def wrap(*args):
+            new_list = []
+            if type == 'str':
+                for element in args:
+                    new_list.append(str(element))
+            if type == 'int':
+                for element in args:
+                    new_list.append(float(element))
+            return function(*new_list)
+        return wrap
+    return wrapper
+
+
+@typed_func(type='str')
+def add_two_symbols(a, b):
+    return a + b
+
+
+print('typed func: add_two_symbols:')
+print(add_two_symbols("3", 5))
+print(add_two_symbols(5, 5))
+print(add_two_symbols('a', 'b'))
+
+
+@typed_func(type='int')
+def add_three_symbols(a, b, c):
+    return a + b + c
+
+
+print('typed func: add_three_symbols:')
+print(add_three_symbols(5, 6, 7))
+print(add_three_symbols("3", 5, 0))
+print(add_three_symbols(0.1, 0.2, 0.4))
