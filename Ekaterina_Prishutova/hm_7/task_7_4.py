850bd0984780951768ab429bdb4bdbcaaa1fd9ec
Ekaterina Prishutova <prishutova.ea@mail.ru>
2022-10-27 22:17:02 +0300

    fix flake8

--- Ekaterina_Prishutova/hm_7/task_7_4.py
+++ Ekaterina_Prishutova/hm_7/task_7_4.py
@@ -15,16 +15,16 @@ def create_lower_upper_letters(language):
         return "".join(([chr(letter) for letter in range(ord("a"), ord("z") + 1)]
                         + [chr(letter) for letter in range(ord("A"), ord("Z") + 1)]))
     elif language == "ru":
-        return ("".join([chr(letter) for letter in range(ord("а"), ord("е") + 1)]) + chr(ord("ё")) +
-                "".join([chr(letter) for letter in range(ord("ж"), ord("я") + 1)]) +
-                "".join([chr(letter) for letter in range(ord("А"), ord("Е") + 1)]) + chr(ord("Ё")) +
-                "".join([chr(letter) for letter in range(ord("Ж"), ord("Я") + 1)]))
+        return ("".join([chr(letter) for letter in range(ord("а"), ord("е") + 1)]) + chr(ord("ё"))
+                + "".join([chr(letter) for letter in range(ord("ж"), ord("я") + 1)])
+                + "".join([chr(letter) for letter in range(ord("А"), ord("Е") + 1)]) + chr(ord("Ё"))
+                + "".join([chr(letter) for letter in range(ord("Ж"), ord("Я") + 1)]))
 
 
 def caesar(input_string, num, language):
     lower_upper_letters = create_lower_upper_letters(language)
     return "".join([lower_upper_letters[
-                        (lower_upper_letters.index(el) + num % len(lower_upper_letters)) % len(lower_upper_letters)]
+                (lower_upper_letters.index(el) + num % len(lower_upper_letters)) % len(lower_upper_letters)]
                     if lower_upper_letters.find(el) >= 0 else el for el in input_string])
 
 
@@ -32,21 +32,20 @@ def kati_encode(input_string, mode, language):
     assert mode in ["encode", "decode"], "Некорректно выбран режим кодирования!"
     lower_upper_letters = create_lower_upper_letters(language)
     if mode == "encode":
-        num = (len(input_string) % len(lower_upper_letters) +
-               int(len(lower_upper_letters) / 4)) % len(lower_upper_letters)
-        return "".join([(lower_upper_letters[((lower_upper_letters.index(el) +
-                                              num % len(lower_upper_letters)) % len(lower_upper_letters))] +
-                        chr(random.randrange(ord("a"), ord("z") + 1)))
+        num = (len(input_string) % len(lower_upper_letters)
+               + int(len(lower_upper_letters) / 4)) % len(lower_upper_letters)
+        return "".join([(lower_upper_letters[((lower_upper_letters.index(el)
+                                              + num % len(lower_upper_letters)) % len(lower_upper_letters))]
+                        + chr(random.randrange(ord("a"), ord("z") + 1)))
                         if lower_upper_letters.find(el) >= 0 else el + chr(random.randrange(ord("a"), ord("z") + 1))
                         for el in input_string[::-1]])
     elif mode == "decode":
         num = -(int(len(input_string) / 2) % len(lower_upper_letters) + int(len(lower_upper_letters) / 4)) % len(
             lower_upper_letters)
         return "".join([lower_upper_letters[
-                            (lower_upper_letters.index(el) + num % len(lower_upper_letters)) % len(lower_upper_letters)]
+                    (lower_upper_letters.index(el) + num % len(lower_upper_letters)) % len(lower_upper_letters)]
                         if lower_upper_letters.find(el) >= 0 else el
                         for el in input_string[-2::-2]])
-
 
 in_str_en = "th!is IS a teSt ]6stri()ng"
 in_str_ru = "Ехал Гpека чеpез pеку, видит Гpека – в pеке pак. Сунул Гpека pуку в pеку, pак за pуку Гpеку цап!"
