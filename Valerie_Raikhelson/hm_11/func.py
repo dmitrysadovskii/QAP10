0eef290dfbdbdafc272eba5ce8a10691fc54aa2d
Raikhelson <valerie.test.dan@gmail.com>
2022-10-29 14:40:40 +0300

    homework_11

---
+++ Valerie_Raikhelson/hm_11/func.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+
+class Methods(ABC):
+    @abstractmethod
+    def addition(self, num_1, num_2):
+        raise NotImplemented
+
+    @abstractmethod
+    def subtraction(self, num_1, num_2):
+        raise NotImplemented
+
+    @abstractmethod
+    def multiplication(self, num_1, num_2):
+        raise NotImplemented
+
+    @abstractmethod
+    def division(self, num_1, num_2):
+        raise NotImplemented
+
+
+class Calculator(Methods):
+    def __init__(self, num_1, num_2):
+        assert isinstance(num_1, (int, float)) and isinstance(num_2, (int, float)), TypeError(
+            "Variables should be defined as an integer or float")
+
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def addition(self):
+        return self.num_1 + self.num_2
+
+    def subtraction(self):
+        return self.num_1 - self.num_2
+
+    def multiplication(self):
+        return self.num_1 * self.num_2
+
+    def division(self):
+        assert self.num_2 != 0, ZeroDivisionError('Divisor cannot be zero')
+        return self.num_1 / self.num_2
---
+++ Valerie_Raikhelson/hm_11/main.py
@@ -0,0 +1,7 @@
+from Valerie_Raikhelson.hm_11.func import Calculator
+
+calc = Calculator(5.8, 0)
+print(calc.addition())
+print(calc.division())
+print(calc.subtraction())
+print(calc.multiplication())
