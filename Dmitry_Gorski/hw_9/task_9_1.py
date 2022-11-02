6274002e1d524155fe24498c55e3f02505cd4d50
dhorski <dhorski@gmail.com>
2022-11-02 16:58:53 +0300

    fix after review

--- Dmitry_Gorski/hw_9/task_9_1.py
+++ Dmitry_Gorski/hw_9/task_9_1.py
@@ -15,17 +15,33 @@ class Visitor:
         self.taken_lst = list()
 
     def get_book(self, book):
-        if book not in self.taken_lst and book.reserved is False:
-            self.taken_lst.append(book)
-        else:
-            print(f'{self.visitor_name} не может взять {book.name}')
+        assert book not in self.taken_lst, print(f'{self.visitor_name} {book.name} и так уже у тебя')
+        assert book.reserved is False, print(f'{self.visitor_name} не может взять {book.name}')
+        self.taken_lst.append(book)
+        book.reserved = True
 
     def reserve_book(self, book):
-        book.reserved = True if book.reserved is False else\
-            print(f'{self.visitor_name} не может зарезервировать {book.name}')
+        assert book.reserved is False, print(f'{self.visitor_name} не может зарезервировать {book.name}')
+        book.reserved = True
 
     def return_book(self, book):
-        if book in self.taken_lst:
-            self.taken_lst.remove(book)
-        else:
-            print(f'{self.visitor_name} не может вернуть {book.name}')
+        assert book in self.taken_lst, print(f'{self.visitor_name} не может вернуть {book.name}')
+        self.taken_lst.remove(book)
+        book.reserved = False
+
+
+book_1 = Book('Книга_1', 'Автор_1', 555, 55555)
+book_2 = Book('Книга_2', 'Автор_2', 666, 77777)
+
+v1 = Visitor('Посетитель_1')
+v2 = Visitor('Посетитель_2')
+
+print(v1.get_book(book_1))
+print(v1.get_book(book_1))
+
+print(v2.get_book(book_1))
+print(v2.reserve_book(book_2))
+print(v1.get_book(book_2))
+
+print(v1.return_book(book_1))
+print(v2.get_book(book_1))
