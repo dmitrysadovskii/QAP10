edbb0f566123eff1c49ffd67bb99a7038e49501b
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-11-06 21:51:17 +0300

    added asserts instead raise, removed return in main.py

--- Alesia Kurylenka/hw_11/main.py
+++ Alesia Kurylenka/hw_11/main.py
@@ -5,28 +5,26 @@ class Calculator(Methods):
 
     @staticmethod
     def validation(number1, number2):
-        if not isinstance(number1, (int, float)):
-            raise print("Numbers should be integer or float")
-        elif not isinstance(number2, (int, float)):
-            raise print("Numbers should be integer or float")
+        assert type(number1) == int or type(number1) == float, "Enter integer or float number"
+        assert type(number2) == int or type(number2) == float, "Enter integer or float number"
 
     def addition(self, number1, number2):
         self.validation(number1, number2)
-        return print(f"Your addition result: {number1 + number2}")
+        print(f"Your addition result: {number1 + number2}")
 
     def subtraction(self, number1, number2):
         self.validation(number1, number2)
-        return print(f"Your subtraction result: {number1 - number2}")
+        print(f"Your subtraction result: {number1 - number2}")
 
     def multiplication(self, number1, number2):
         self.validation(number1, number2)
-        return print(f"Your multiplication result: {number1 * number2}")
+        print(f"Your multiplication result: {number1 * number2}")
 
     def division(self, number1, number2):
         self.validation(number1, number2)
         if int(number2) == 0:
             raise ZeroDivisionError("Please enter number not equal to zero")
-        return print(f"Your division result: {number1 / number2}")
+        print(f"Your division result: {number1 / number2}")
 
 
 calc = Calculator()
