db2c0b2c4e3d3769a86ccd51d72d2b97534be7b4
Elena Rybina <elenarybinastudio@gmail.com>
2022-11-06 22:11:45 -0800

    add hw_6

---
+++ Elena Rybina/hw_6/task_6_3.py
@@ -0,0 +1,20 @@
+
+choice = input(" Select an action:\n 1 - Addition\n 2 - Subtraction\n 3 - Multiplication\n 4 - Division\nEnter Your choice: ")
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+
+def calculater(choice, a, b):
+
+    if choice == 1:
+        return a+b
+    elif choice == 2:
+        return a-b
+    elif choice == 3:
+        return a*b
+    elif choice == 4:
+        return a/b
+    else:
+        return
+
+print(calculater(int(choice), float(a), float(b)))
