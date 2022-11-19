e4be91b5dafa7aa5a3f2bc9983ffa82fdfac4c71
Evgeny Sporshev <verygnawtyatwork@yandex.ru>
2022-11-20 00:50:56 +0300

    Evgeny Sporshev Homework 7

---
+++ Evgeny Sporshev/hw_7/task_7_4.py
@@ -0,0 +1,43 @@
+# Шифр цезаря
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+example_one = 'hello world!'
+example_two = 'this is a test string'
+
+
+def encode(some_string, step):
+    result = ''
+    for i in some_string:
+        if i in alphabet:
+            pos = 0
+            new_pos = 0
+            position = alphabet.find(i)
+            new_pos = alphabet[position + step]
+            result += new_pos
+        else:
+            result += i
+    return result
+
+
+def decode(some_string, step):
+    result = ''
+    for i in some_string:
+        if i in alphabet:
+            pos = 0
+            new_pos = 0
+            position = alphabet.find(i)
+            new_pos = alphabet[position - step]
+            result += new_pos
+        else:
+            result += i
+    return result
+
+
+action_one = encode(example_one, 1)
+print(f'Результат кодирования строки {example_one} : {action_one}')
+action_two = decode(action_one, 1)
+print(f'Результат декодирования строки {action_one} : {action_two}')
+
+action_one = encode(example_two, 1)
+print(f'Результат кодирования строки {example_two} : {action_one}')
+action_two = decode(action_one, 1)
+print(f'Результат декодирования строки {action_one} : {action_two}')
