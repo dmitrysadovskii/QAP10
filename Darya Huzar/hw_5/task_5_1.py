7f11823b86e54ed44fa9b4b7a456869a994fa0a5
Darya Huzar <darya.huzar@gmail.com>
2023-01-31 22:54:22 +0300

    homework 5, task 1 fixed

--- Darya Huzar/hw_5/task_5_1.py
+++ Darya Huzar/hw_5/task_5_1.py
@@ -1,22 +1,39 @@
-likes = ['Анна', 'Алекс', 'Марк', 'Петя', 'Антон']
+import langid
 
-if len(likes) == 0:
-    print('No one likes this')
-elif len(likes[0]) == len(likes[0].encode()):
-    if len(likes) == 1:
-        print(likes[0], 'likes this')
-    elif len(likes) == 2:
-        print(f'{likes[0]} and {likes[1]} like this')
-    elif len(likes) == 3:
-        print(f'{likes[0]}, {likes[1]} and {likes[2]} like this')
-    else:
-        print(f'{likes[0]}, {likes[1]} and {len(likes) - 2} others like this')
+names = input("Введите имена/Enter names: ")
+array = names.split()
+array_lang = langid.classify(names)
+
+
+def en_names():
+    if len(array) == 0:
+        print("no one likes this")
+    elif len(array) == 1:
+        print(array[0], "likes this")
+    elif len(array) == 2:
+        print(array[0], "and", array[1], "like this")
+    elif len(array) == 3:
+        print(array[0], array[1], "and", array[2], "like this")
+    elif len(array) > 3:
+        print(array[0], array[1], "and", len(array) - 2, "others like this")
+
+
+def ru_names():
+    if len(array) == 0:
+        print("это никому не нравится")
+    elif len(array) == 1:
+        print(array[0], "нравится это")
+    elif len(array) == 2:
+        print(array[0], "и", array[1], "нравится это")
+    elif len(array) == 3:
+        print(array[0], array[1], "и", array[2], "нравится это")
+    elif len(array) > 3:
+        print(array[0], array[1], "и", len(array) - 2, "другим нравится это")
+
+
+if array_lang[0] == 'en':
+    en_names()
+elif array_lang[0] == 'ru':
+    ru_names()
 else:
-    if len(likes) == 1:
-        print(likes[0], 'нравится это')
-    elif len(likes) == 2:
-        print(f'{likes[0]} и {likes[1]} нравится это')
-    elif len(likes) == 3:
-        print(f'{likes[0]}, {likes[1]} и {likes[2]} нравится это')
-    else:
-        print(f'{likes[0]}, {likes[1]} и {len(likes) - 2} другим нравится это')
+    print("Invalid language")
